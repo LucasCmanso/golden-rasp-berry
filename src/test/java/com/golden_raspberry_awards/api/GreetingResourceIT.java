46e996c6ba14c149fde86eb1771eba3582e6e68d
Lucas <lucas.cmanso@outlook.com>
2023-12-16 11:57:48 -0300

    Initial Commit

---
+++ src/test/java/com/golden_raspberry_awards/api/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.golden_raspberry_awards.api;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
