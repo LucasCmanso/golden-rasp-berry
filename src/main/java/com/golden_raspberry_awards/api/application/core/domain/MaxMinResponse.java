95f44694c744dbcb01532687d129a0e4ca8e6f97
Lucas <lucas.cmanso@outlook.com>
2023-12-17 22:40:44 -0300

    Ajust min -> max

--- src/main/java/com/golden_raspberry_awards/api/application/core/domain/MaxMinResponse.java
+++ src/main/java/com/golden_raspberry_awards/api/application/core/domain/MaxMinResponse.java
@@ -3,23 +3,16 @@ package com.golden_raspberry_awards.api.application.core.domain;
 import java.util.List;
 
 public class MaxMinResponse {
-    private List<RangeOfWinnersResponse> max;
     private List<RangeOfWinnersResponse> min;
+    private List<RangeOfWinnersResponse> max;
 
     public MaxMinResponse(){};
 
-    public MaxMinResponse(List<RangeOfWinnersResponse> max, List<RangeOfWinnersResponse> min) {
-        this.max = max;
+    public MaxMinResponse(List<RangeOfWinnersResponse> min, List<RangeOfWinnersResponse> max) {
         this.min = min;
-    }
-
-    public List<RangeOfWinnersResponse> getMax() {
-        return max;
-    }
-
-    public void setMax(List<RangeOfWinnersResponse> max) {
         this.max = max;
     }
+
 
     public List<RangeOfWinnersResponse> getMin() {
         return min;
@@ -27,5 +20,12 @@ public class MaxMinResponse {
 
     public void setMin(List<RangeOfWinnersResponse> min) {
         this.min = min;
+    }
+    public List<RangeOfWinnersResponse> getMax() {
+        return max;
+    }
+
+    public void setMax(List<RangeOfWinnersResponse> max) {
+        this.max = max;
     }
 }
